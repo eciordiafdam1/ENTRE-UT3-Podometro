9669062eb8523c71010b71ca07e7ff5ee2c3fc47
Endika Ciordia <eciordiafdam1@gmail.com>
2021-10-14 16:45:42 +0200

    Identificar si es hombre o mujer. Calcular las zancadas que da.

--- Podometro.java
+++ Podometro.java
@@ -8,132 +8,132 @@
 public class Podometro  {
         
     
-    // /**
-     // * Inicializa el podómetro con la marca indicada por el parámetro.
-     // * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
-     // */
-    // public if sexo(hombre o mujer){
-        // sexoMujer="M";
+    /**
+     * Inicializa el podómetro con la marca indicada por el parámetro.
+     * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
+     */
+    public if sexo(hombre o mujer){
+        sexoMujer="M";
         
         
     
     
-// }
+}
 
-// else sexo(hombre o mujer){
-    // sexoHombre= "H";
-// }
+else sexo(hombre o mujer){
+    sexoHombre= "H";
+}
    
-    // public int (LongitudzancadaHombre){
-        // 0,45*45%=0,2025;
-    // }
+    public int (LongitudzancadaHombre){
+        0,45*45%=0,2025;
+    }
     
-    // public int (zancadaMujer){
-        // 0,41*41%=0,1681;
-    // }
+    public int (zancadaMujer){
+        0,41*41%=0,1681;
+    }
     
-    // public String (Sabado){
-         // sabado=6   
-    // }
+    public String (Sabado){
+         sabado=6   
+    }
     
-    // public String (Domingo){
-         // Domingo=7   
-    // }
+    public String (Domingo){
+         Domingo=7   
+    }
     
-    // /**
-     // * accesor para la marca
-     // *  
-     // */
-    // public void getMarca(Hombres 41% Mujeres 45%) {
+    /**
+     * accesor para la marca
+     *  
+     */
+    public void getMarca(Hombres 41% Mujeres 45%) {
 
-         // Math.sqrt(41%/100)=0,41
-         // Math.sqrt(45%/100)=0,45
+         Math.sqrt(41%/100)=0,41
+         Math.sqrt(45%/100)=0,45
 
-    // }
+    }
 
-    // /**
-     // * Simula la configuración del podómetro.
-     // * Recibe como parámetros la altura y el sexo de una persona
-     // * y asigna estos valores a los atributos correspondiente.
-     // * Asigna además el valor adecuado al atributo longitudZancada
-     // * 
-     // * (leer enunciado)
-     // *  
-     // */
-    // public void configurar(double queAltura, char queSexo) {
+    /**
+     * Simula la configuración del podómetro.
+     * Recibe como parámetros la altura y el sexo de una persona
+     * y asigna estos valores a los atributos correspondiente.
+     * Asigna además el valor adecuado al atributo longitudZancada
+     * 
+     * (leer enunciado)
+     *  
+     */
+    public void configurar(double queAltura, char queSexo) {
 
         
-    // }
+    }
 
-     // /**
-     // *  Recibe cuatro parámetros que supondremos correctos:
-     // *    pasos - el nº de pasos caminados
-     // *    dia - nº de día de la semana en que se ha hecho la caminata 
-     // *              (1 - Lunes, 2 - Martes - .... - 6 - Sábado, 7 - Domingo)
-     // *    horaInicio – hora de inicio de la caminata
-     // *    horaFin – hora de fin de la caminata
-     // *    
-     // *    A partir de estos parámetros el método debe realizar ciertos cálculos
-     // *    y  actualizará el podómetro adecuadamente  
-     // *   
-     // *   (leer enunciado del ejercicio)
-     // */
-    // public void registrarCaminata(int pasos, int dia, int horaInicio,
-                            // int horaFin) {
+     /**
+     *  Recibe cuatro parámetros que supondremos correctos:
+     *    pasos - el nº de pasos caminados
+     *    dia - nº de día de la semana en que se ha hecho la caminata 
+     *              (1 - Lunes, 2 - Martes - .... - 6 - Sábado, 7 - Domingo)
+     *    horaInicio – hora de inicio de la caminata
+     *    horaFin – hora de fin de la caminata
+     *    
+     *    A partir de estos parámetros el método debe realizar ciertos cálculos
+     *    y  actualizará el podómetro adecuadamente  
+     *   
+     *   (leer enunciado del ejercicio)
+     */
+    public void registrarCaminata(int pasos, int dia, int horaInicio,
+                            int horaFin) {
 
        
 
-    // }
+    }
     
-     // /**
-     // * Muestra en pantalla la configuración del podómetro
-     // * (altura, sexo y longitud de la zancada)
-     // * 
-     // * (ver enunciado)
-     // *  
-     // */
-    // public void printConfiguracion() {
+     /**
+     * Muestra en pantalla la configuración del podómetro
+     * (altura, sexo y longitud de la zancada)
+     * 
+     * (ver enunciado)
+     *  
+     */
+    public void printConfiguracion() {
 
         
 
-    // }
+    }
 
-    // /**
-     // * Muestra en pantalla información acerca de la distancia recorrida,
-     // * pasos, tiempo total caminado, ....
-     // * 
-     // * (leer enunciado)
-     // *  
-     // */
-    // public void printEstadísticas() {
+    /**
+     * Muestra en pantalla información acerca de la distancia recorrida,
+     * pasos, tiempo total caminado, ....
+     * 
+     * (leer enunciado)
+     *  
+     */
+    public void printEstadísticas() {
 
         
 
-    // }
+    }
 
    
 
-    // /**
-     // *  Calcula y devuelve un String que representa el nombre del día
-     // *  en el que se ha caminado más pasos - "SÁBADO"   "DOMINGO" o  "LABORABLES"
-     // */
-    // public String diaMayorNumeroPasos() {
+    /**
+     *  Calcula y devuelve un String que representa el nombre del día
+     *  en el que se ha caminado más pasos - "SÁBADO"   "DOMINGO" o  "LABORABLES"
+     */
+    public String diaMayorNumeroPasos() {
 
          
 
-    // }
+    }
     
     
-    // /**
-     // * Restablecer los valores iniciales del podómetro
-     // * Todos los atributos se ponen a cero salvo el sexo
-     // * que se establece a MUJER. La marca no varía
-     // *  
-     // */    
-    // public void reset() {
+    /**
+     * Restablecer los valores iniciales del podómetro
+     * Todos los atributos se ponen a cero salvo el sexo
+     * que se establece a MUJER. La marca no varía
+     *  
+     */    
+    public void reset() {
 
         
 
-    // }
+    }
 
 }
